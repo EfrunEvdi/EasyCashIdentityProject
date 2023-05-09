cb12c8048193866f35cc14212c749db9baa50aa1
efrunevdi <efruncetkin@gmail.com>
2023-05-09 20:54:14 +0300

    9. Ders Sonu

---
+++ EasyCashIdentityProject.BusinessLayer/Concrete/CustomerAccountProcessManager.cs
@@ -0,0 +1,45 @@
+﻿using EasyCashIdentityProject.DataAccessLayer.Abstract;
+using EasyCashIdentityProject.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.BusinessLayer.Concrete
+{
+    public class CustomerAccountProcessManager : ICustomerAccountProcessDal
+    {
+        private readonly ICustomerAccountProcessDal _customerAccountProcessDal;
+
+        public CustomerAccountProcessManager(ICustomerAccountProcessDal customerAccountProcessDal)
+        {
+            _customerAccountProcessDal = customerAccountProcessDal;
+        }
+
+        public void Delete(CustomerAccountProcess t)
+        {
+            _customerAccountProcessDal.Delete(t);
+        }
+
+        public CustomerAccountProcess GetByID(int id)
+        {
+            return _customerAccountProcessDal.GetByID(id);
+        }
+
+        public List<CustomerAccountProcess> GetList()
+        {
+            return _customerAccountProcessDal.GetList();
+        }
+
+        public void Insert(CustomerAccountProcess t)
+        {
+            _customerAccountProcessDal.Insert(t);
+        }
+
+        public void Update(CustomerAccountProcess t)
+        {
+            _customerAccountProcessDal.Update(t);
+        }
+    }
+}
