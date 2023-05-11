b8f5d44c08c19ff35021c7d238bcba04a1c957aa
efrunevdi <efruncetkin@gmail.com>
2023-05-11 21:08:50 +0300

    11. Ders Sonu

--- EasyCashIdentityProject.PresentationLayer/Controllers/RegisterController.cs
+++ EasyCashIdentityProject.PresentationLayer/Controllers/RegisterController.cs
@@ -30,15 +30,37 @@ namespace EasyCashIdentityProject.PresentationLayer.Controllers
                     UserName = appUserRegisterDto.Username,
                     Name = appUserRegisterDto.Name,
                     Surname = appUserRegisterDto.Surname,
-                    Email = appUserRegisterDto.Email
+                    Email = appUserRegisterDto.Email,
+                    City = "İstanbul",
+                    District = "B",
+                    ImageUrl = "C"
                 };
+
                 var result = await _userManager.CreateAsync(appUser, appUserRegisterDto.Password);
+
                 if (result.Succeeded)
                 {
                     return RedirectToAction("Index", "ConfirmMail");
+                }
+
+                else
+                {
+                    foreach (var item in result.Errors)
+                    {
+                        ModelState.AddModelError("", item.Description);
+                    }
                 }
             }
             return View();
         }
     }
 }
+
+/*
+ * Şifre
+ * En az 6 karakterden oluşacak
+ * En az bir küçük harf
+ * En az bir büyük harf
+ * En az bir sembol
+ * En az bir sayı içermeli
+ */
