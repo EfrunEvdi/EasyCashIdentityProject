245a12a50436bf7810f98e627bb3513ef41979d0
efrunevdi <efruncetkin@gmail.com>
2023-05-12 16:41:41 +0300

    12. Ders Sonu

---
+++ EasyCashIdentityProject.PresentationLayer/Models/CustomIdentityValidator.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace EasyCashIdentityProject.PresentationLayer.Models
+{
+    public class CustomIdentityValidator : IdentityErrorDescriber
+    {
+        public override IdentityError PasswordTooShort(int length)
+        {
+            return new IdentityError()
+            {
+                Code = "PasswordTooShort",
+                Description = $"Şifre en az {length} karakter olmalıdır."
+            };
+        }
+
+        public override IdentityError PasswordRequiresUpper()
+        {
+            return new IdentityError()
+            {
+                Code = "PasswordRequiresUpper",
+                Description = "Şifre en az bir büyük harf içermelidir."
+            };
+        }
+
+        public override IdentityError PasswordRequiresLower()
+        {
+            return new IdentityError()
+            {
+                Code = "PasswordRequiresLower",
+                Description = "Şifre en az bir küçük harf içermelidir."
+            };
+        }
+
+        public override IdentityError PasswordRequiresDigit()
+        {
+            return new IdentityError()
+            {
+                Code = "PasswordRequiresDigit",
+                Description = "Şifre en az bir rakam içermelidir."
+            };
+        }
+
+        public override IdentityError PasswordRequiresNonAlphanumeric()
+        {
+            return new IdentityError()
+            {
+                Code = "PasswordRequiresNonAlphanumeric",
+                Description = "Şifre en az bir alfanümerik olmayan karakter içermelidir."
+            };
+        }
+    }
+}
+
+/*
+  IdentityError seçenekleri şunlardır:
+
+DefaultError: Bu, IdentityError'un varsayılan hatasıdır. Kimlik doğrulama hatası oluştuğunda bu mesaj kullanıcılara gösterilir.
+
+DuplicateEmail: Kullanıcı kaydı sırasında e-posta adresi zaten kayıtlıysa bu hata oluşur.
+
+DuplicateUserName: Kullanıcı adı zaten mevcut olduğunda bu hata oluşur.
+
+InvalidEmail: Geçersiz bir e-posta adresi girildiğinde bu hata oluşur.
+
+InvalidUserName: Geçersiz bir kullanıcı adı girildiğinde bu hata oluşur.
+
+PasswordMismatch: Kullanıcının girdiği şifreler eşleşmiyorsa bu hata oluşur.
+
+PasswordRequiresDigit: Şifre en az bir rakam içermelidir.
+
+PasswordRequiresLower: Şifre en az bir küçük harf içermelidir.
+
+PasswordRequiresNonAlphanumeric: Şifre en az bir alfanümerik olmayan karakter içermelidir.
+
+PasswordRequiresUpper: Şifre en az bir büyük harf içermelidir.
+
+PasswordTooShort: Şifre belirlenen minimum uzunlukta değilse bu hata oluşur.
+ */
