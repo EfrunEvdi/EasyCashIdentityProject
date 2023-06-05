8f69cc14243d0f0c1f94cc83bdd31657cc31dd27
efrunevdi <efruncetkin@gmail.com>
2023-06-05 18:35:42 +0300

    23. Ders Sonu

---
+++ EasyCashIdentityProject.DataAccessLayer/Migrations/20230605151735_mid_add_customer_relation_proccess.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EasyCashIdentityProject.DataAccessLayer.Migrations
+{
+    public partial class mid_add_customer_relation_proccess : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ReceiverID",
+                table: "CustomerAccountsProcess",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "SenderID",
+                table: "CustomerAccountsProcess",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CustomerAccountsProcess_ReceiverID",
+                table: "CustomerAccountsProcess",
+                column: "ReceiverID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CustomerAccountsProcess_SenderID",
+                table: "CustomerAccountsProcess",
+                column: "SenderID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CustomerAccountsProcess_CustomerAccounts_ReceiverID",
+                table: "CustomerAccountsProcess",
+                column: "ReceiverID",
+                principalTable: "CustomerAccounts",
+                principalColumn: "CustomerAccountID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CustomerAccountsProcess_CustomerAccounts_SenderID",
+                table: "CustomerAccountsProcess",
+                column: "SenderID",
+                principalTable: "CustomerAccounts",
+                principalColumn: "CustomerAccountID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CustomerAccountsProcess_CustomerAccounts_ReceiverID",
+                table: "CustomerAccountsProcess");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_CustomerAccountsProcess_CustomerAccounts_SenderID",
+                table: "CustomerAccountsProcess");
+
+            migrationBuilder.DropIndex(
+                name: "IX_CustomerAccountsProcess_ReceiverID",
+                table: "CustomerAccountsProcess");
+
+            migrationBuilder.DropIndex(
+                name: "IX_CustomerAccountsProcess_SenderID",
+                table: "CustomerAccountsProcess");
+
+            migrationBuilder.DropColumn(
+                name: "ReceiverID",
+                table: "CustomerAccountsProcess");
+
+            migrationBuilder.DropColumn(
+                name: "SenderID",
+                table: "CustomerAccountsProcess");
+        }
+    }
+}
--- EasyCashIdentityProject.PresentationLayer/Controllers/MyAccountsController.cs
+++ EasyCashIdentityProject.PresentationLayer/Controllers/MyAccountsController.cs
@@ -35,6 +35,31 @@ namespace EasyCashIdentityProject.PresentationLayer.Controllers
         [HttpPost]
         public async Task<IActionResult> Index(AppUserEditDto appUserEditDto)
         {
+            if (appUserEditDto.Password == appUserEditDto.ConfirmPassword)
+            {
+                var user = await _userManager.FindByNameAsync(User.Identity.Name);
+                user.Name = appUserEditDto.Name;
+                user.Surname = appUserEditDto.Surname;
+                user.District = appUserEditDto.District;
+                user.City = appUserEditDto.City;
+                user.PhoneNumber = appUserEditDto.PhoneNumber;
+                user.ImageUrl = "sa";
+                user.Email = appUserEditDto.Email;
+                user.PasswordHash = _userManager.PasswordHasher.HashPassword(user, appUserEditDto.Password);
+                var result = await _userManager.UpdateAsync(user);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Login");
+                }
+            }
+
+            else
+            {
+                return View(appUserEditDto);
+            }
+
+
             return View();
         }
     }
